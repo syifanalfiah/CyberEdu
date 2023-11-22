411f4f7d7276c66b71b39f3d9c35d072575012d1
syifanalfiah <112307944+syifanalfiah@users.noreply.github.com>
2023-11-22 22:48:42 +0700

    Update index.js

--- client/src/constant/index.js
+++ client/src/constant/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-
-const Loader = () => (
-  <div role="status">
-    <svg aria-hidden="true" className="inline w-10 h-10 mr-2 text-gray-200 animate-spin fill-[#8e7cc3]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-    </svg>
-  </div>
-);
-
-export default Loader;
+export const CyberPrompts = [
+    'Create a visual illustration of how firewalls work to protect a system.',
+    'Design a network security architecture diagram highlighting layers of protection.',
+    'Illustrate cyber attack scenarios and the defense steps taken.',
+    'Generate an infographic explaining the concept of data encryption.',
+    'Visualize the process of threat detection and response.',
+    'Craft a network map highlighting vulnerable points and ways to secure them.',
+    'Create an illustration depicting the roles of security technologies such as antivirus and antispyware.',
+    'Design concepts of security at the application, system, and network levels.',
+    'Illustrate identity and access-based security model.',
+    'Create a flowchart to demonstrate the system security testing process.',
+    'Design a security schema for secure software development, including Linux.',
+    'Create an infographic on phishing attacks and prevention methods.',
+    'Visualize the principles of security in operating systems, including Linux.',
+    'Generate a map illustrating common cybersecurity threats.',
+    'Illustrate the security concepts of cloud computing.',
+    'Design steps for security observation and log analysis.',
+    'Create a heatmap of common cybersecurity threats.',
+    'Illustrate the concept of sandboxing to isolate threats.',
+    'Design the process of system security auditing.',
+    'Create a security diagram for Internet of Things (IoT).',
+    'Illustrate security concepts in open-source software development.',
+  ];
+  
+  
